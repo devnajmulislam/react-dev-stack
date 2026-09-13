33f1949d48f794c6354fda8898b520034bb71a5a
Najmul Islam <itzmenajmul@gmail.com>
2026-09-13 11:39:03 +0600

    typrography and styles update for Banner section

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,27 +4,39 @@ const Banner = () => {
   return (
     <>
       {/* Banner parent */}
-      <div className="flex justify-between items-center container mx-auto my-10">
+      <div className="grid grid-cols-12 justify-between items-center container mx-auto my-20">
         {/* Banner left site */}
-        <div>
-          <h1 className="text-5xl font-bold">
-            Build Your Ideal<span>Development Stack</span>
+        <div className="col-span-7">
+          <h1 className="text-5xl text-slate-900 md:text-6xl font-bold">
+            Build Your Ideal
+            <br />
+            <span className="bg-gradient-to-r from-orange-500 via-pink-600 to-purple-600 bg-clip-text text-transparent">
+              Development Stack
+            </span>
           </h1>
-          <p className="text-2xl py-2">
+          <p className="mt-6 text-lg text-slate-600 max-w-1/2">
             Explore frontend, backend, database, and tooling options, compare
             them side by side, and put together the stack that fits your next
             project.
           </p>
           {/* Banner btn container */}
-          <div className="">
-            <button className="btn btn-secondary">Explore Technologies</button>
-            <button className="btn">Learn More</button>
+          <div className="mt-8 flex flex-wrap items-center gap-4 ">
+            <button className="cursor-pointer rounded-lg bg-gradient-to-r from-orange-500 to-pink-600 px-6 py-3 text-sm font-semibold text-white shadow-md transition-all hover:opacity-90">
+              Explore Technologies
+            </button>
+            <button className="cursor-pointer rounded-lg border border-slate-200 bg-white px-6 py-3 text-sm font-semibold text-slate-700 shadow-sm transition-all hover:bg-slate-50">
+              Learn More
+            </button>
           </div>
         </div>
 
         {/* Banner right site */}
-        <div>
-          <img src={BannerImg} alt="Banner right site image" />
+        <div className="col-span-5 w-full max-w-md lg:max-w-lg flex justify-center">
+          <img
+            src={BannerImg}
+            alt="Banner right site image"
+            className="w-full h-auto object-contain"
+          />
         </div>
       </div>
     </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import NavLogo from "../assets/logo-text.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar-sticky border-b border-[#ddd3d3d5]">
+    <nav className="navbar-sticky border-b border-[#ddd3d3d5]  backdrop-blur-sm">
       {/* nav parent */}
       <div className="flex justify-between items-center container mx-auto py-[30px] px-2 ">
         {/* Navbar left site */}
