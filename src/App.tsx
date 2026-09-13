d02e5197eb74d6e998b7d38f9e108eee1440ff09
Najmul Islam <itzmenajmul@gmail.com>
2026-09-13 11:06:49 +0600

    update typography for Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { Suspense, use } from "react";
+import { Suspense } from "react";
 import Banner from "./components/Banner";
 import ExpTechnologies from "./components/ExpTechnologies";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
-
-     const technologiesPromise = async() =>{
-    const res = await fetch('/technologies.json');
+  // Technologies data promise
+  const technologiesPromise = async () => {
+    const res = await fetch("/technologies.json");
     const data = await res.json();
     return data;
-  }
+  };
 
-  // const technologies = use(technologiesPromise());
-  // console.log(technologies)
-
-
-  return <>
-  <Navbar/>
-  <Banner/>
-  <Suspense fallback={<span className="loading loading-spinner text-primary"></span>}>
-    <ExpTechnologies technologiesPromise={technologiesPromise()}/>
-  </Suspense>
-  <Footer/>
-  </>;
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Suspense
+        fallback={
+          <span className="loading loading-spinner text-primary"></span>
+        }
+      >
+        <ExpTechnologies technologiesPromise={technologiesPromise()} />
+      </Suspense>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,9 @@ import NavLogo from "../assets/logo-text.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar-sticky">
-        {/* nav parent */}
-      <div className="bg-orange-200 flex justify-between items-center container mx-auto py-4 px-2 ">
+    <nav className="navbar-sticky border-b border-[#ddd3d3d5]">
+      {/* nav parent */}
+      <div className="flex justify-between items-center container mx-auto py-[30px] px-2 ">
         {/* Navbar left site */}
         <div>
           <img src={NavLogo} alt="Nav logo" />
@@ -12,19 +12,35 @@ const Navbar = () => {
 
         {/* Navbar middle site */}
         <div>
-            <ul className="flex items-center gap-4">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Technologies</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
+          <ul className="flex items-center gap-4 text-[14px] font-semibold text-[#475569]">
+            <li className="font-hover-color">
+              <a className="text-[#DB2777]" href="#">
+                Home
+              </a>
+            </li>
+            <li className="font-hover-color">
+              <a href="#">Technologies</a>
+            </li>
+            <li className="font-hover-color">
+              <a href="#">Projects</a>
+            </li>
+            <li className="font-hover-color">
+              <a href="#">About</a>
+            </li>
+            <li className="font-hover-color">
+              <a href="#">Contact</a>
+            </li>
+          </ul>
         </div>
 
         {/* Navbar right site */}
-        <div>
-             <button>Sign In</button>
-             <button className="btn btn-active btn-secondary">Sign Up</button>
+        <div className="text-[14px] font-semibold text-[#475569]">
+          <button className="mr-6 cursor-pointer btn-hover-style">
+            Sign In
+          </button>
+          <button className="btn btn-active btn-secondary rounded-4xl cursor-pointer hover:bg-[#a71255]">
+            Sign Up
+          </button>
         </div>
       </div>
     </nav>
